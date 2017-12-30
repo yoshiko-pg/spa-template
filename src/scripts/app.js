26536639776d0ee14049b387ee740235cbd50e78
maasa <maasa.giss2@gmail.com>
2017-12-31 00:32:42 +0900

    :up: Add react-dom

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,10 @@
+/* eslint-env browser */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import 'ress/ress.css'
 import '../styles/base.css'
+
+import RootComponent from './index.js'
+
+ReactDOM.render(<RootComponent />, document.getElementById('app'))
---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>Hello, World.</h1>
+    <p>Rendering by react</p>
+  </div>
+)
